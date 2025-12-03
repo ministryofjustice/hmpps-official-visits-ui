5fd6a5f877ca8fcfbcd7211a58891ce2230d4aec
Jack Reeve <jack.reeve@justice.gov.uk>
2025-12-03 15:18:53 +0000

    OV-186: Remove cypress deps

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,8 +4,6 @@ export default configureAllowedScripts({
   allowlist: {
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
-    // Needed by Cypress for pulling in the Cypress binary
-    'node_modules/cypress@15.6.0': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
