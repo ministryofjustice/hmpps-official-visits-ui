f062e4b315ec6825fcb9339a9b064c4ce5b31f70
Jack Reeve <jack.reeve@justice.gov.uk>
2026-04-15 18:12:19 +0100

    increase screenshot size for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     trace: process.env.CI ? 'off' : 'on',
     ...devices['Desktop Chrome'],
+    viewport: { width: 1920, height: 1920 },
     testIdAttribute: 'data-qa',
     baseURL: 'http://localhost:3007',
   },
