00c0bf161ccf755990503885bd156e6ad5e77a98
Jack Reeve <jack.reeve@justice.gov.uk>
2026-05-05 14:37:27 +0100

    test

--- server/middleware/journey/preventNavigationToMismatchedCaseloadsJourneys.ts
+++ server/middleware/journey/preventNavigationToMismatchedCaseloadsJourneys.ts
@@ -5,7 +5,7 @@ export default function preventNavigationToMismatchedCaseloadsJourneys(): Reques
     const journeyId = req.originalUrl?.match(/\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/i)?.[1]
     const journey = req.session.journeyData?.[journeyId]
     const caseLoadMatches = journey?.officialVisit?.caseLoad === req.session.activeCaseLoadId
-    if (journey && req.session.activeCaseLoadId && !caseLoadMatches) {
+    if (journey && req.session.activeCaseLoadId && journey?.officialVisit?.caseLoad && !caseLoadMatches) {
       return res.redirect('/')
     }
 
