84b6138d78f2d683839815ce4e352b8e98a5459b
JackReeveMoJ <jack.reeve@justice.gov.uk>
2026-05-12 15:47:55 +0100

    Upgrade to typescript 6 (#210)

--- server/routes/testutils/cheerio.ts
+++ server/routes/testutils/cheerio.ts
@@ -1,23 +1,23 @@
-import Root = cheerio.Root
+import type { CheerioAPI } from 'cheerio'
 
-export const getPageHeader = ($: Root) => $('h1').first().text().trim()
-export const getByDataQa = ($: Root, dataQa: string) => $(`[data-qa=${dataQa}]`)
-export const existsByDataQa = ($: Root, dataQa: string) => getByDataQa($, dataQa).length > 0
-export const getByName = ($: Root, name: string) => $(`[name=${name}]`)
-export const getTextById = ($: Root, id: string) => $(`[id=${id}]`).text().trim()
-export const existsByName = ($: Root, name: string) => getByName($, name).length > 0
-export const getPageAlert = ($: Root) => $('div.moj-alert__content').text().trim()
+export const getPageHeader = ($: CheerioAPI) => $('h1').first().text().trim()
+export const getByDataQa = ($: CheerioAPI, dataQa: string) => $(`[data-qa=${dataQa}]`)
+export const existsByDataQa = ($: CheerioAPI, dataQa: string) => getByDataQa($, dataQa).length > 0
+export const getByName = ($: CheerioAPI, name: string) => $(`[name=${name}]`)
+export const getTextById = ($: CheerioAPI, id: string) => $(`[id=${id}]`).text().trim()
+export const existsByName = ($: CheerioAPI, name: string) => getByName($, name).length > 0
+export const getPageAlert = ($: CheerioAPI) => $('div.moj-alert__content').text().trim()
 
-export const getByLabel = ($: Root, label: string) => {
+export const getByLabel = ($: CheerioAPI, label: string) => {
   const lbl = $(`label:contains("${label}")`)
   return lbl.attr('for') ? $(`#${lbl.attr('for')}`) : lbl.find('input, select, textarea')
 }
-export const existsByLabel = ($: Root, label: string) => getByLabel($, label).length > 0
+export const existsByLabel = ($: CheerioAPI, label: string) => getByLabel($, label).length > 0
 
-export const getValueByKey = ($: Root, key: string, index: number = 0) => {
+export const getValueByKey = ($: CheerioAPI, key: string, index: number = 0) => {
   return (
     $('.govuk-summary-list .govuk-summary-list__row')
-      .filter((_, e) => $(e).find('.govuk-summary-list__key').text().trim() === key)
+      .filter((_: number, e) => $(e).find('.govuk-summary-list__key').text().trim() === key)
       .find('.govuk-summary-list__value')
       .eq(index)
       .text()
@@ -25,10 +25,10 @@ export const getValueByKey = ($: Root, key: string, index: number = 0) => {
   )
 }
 
-export const getActionsByKey = ($: Root, key: string, index: number = 0, childIndex: number = 0) => {
+export const getActionsByKey = ($: CheerioAPI, key: string, index: number = 0, childIndex: number = 0) => {
   return (
     $('.govuk-summary-list .govuk-summary-list__row')
-      .filter((_, e) => $(e).find('.govuk-summary-list__key').text().trim() === key)
+      .filter((_: number, e) => $(e).find('.govuk-summary-list__key').text().trim() === key)
       .find('.govuk-summary-list__actions')
       .eq(index)
       .children()
@@ -36,55 +36,55 @@ export const getActionsByKey = ($: Root, key: string, index: number = 0, childIn
   )
 }
 
-export const existsByKey = ($: Root, key: string) => {
+export const existsByKey = ($: CheerioAPI, key: string) => {
   return (
     $('.govuk-summary-list .govuk-summary-list__row').filter(
-      (_, e) => $(e).find('.govuk-summary-list__key').text().trim() === key,
+      (_: number, e) => $(e).find('.govuk-summary-list__key').text().trim() === key,
     ).length > 0
   )
 }
 
-export const dropdownOptions = ($: Root, name: string) => {
+export const dropdownOptions = ($: CheerioAPI, name: string) => {
   return getByName($, name)
     .find('option')
-    .map((_, option) => $(option).attr('value'))
+    .map((_: number, option) => $(option).attr('value'))
     .get()
     .filter(s => s.length > 1)
 }
 
-export const radioOptions = ($: Root, name: string) => {
+export const radioOptions = ($: CheerioAPI, name: string) => {
   return getByName($, name)
-    .map((_, option) => $(option).attr('value'))
+    .map((_: number, option) => $(option).attr('value'))
     .get()
     .filter(s => s.length > 1)
 }
 
-export const getMiniProfile = ($: Root) => {
+export const getMiniProfile = ($: CheerioAPI) => {
   return getByDataQa($, 'mini-profile')
 }
 
-export const getProgressTrackerLabels = ($: Root) => {
+export const getProgressTrackerLabels = ($: CheerioAPI) => {
   return $('.moj-progress-bar > ol > li')
 }
 
-export const getProgressTrackerItems = ($: Root) => {
+export const getProgressTrackerItems = ($: CheerioAPI) => {
   return $('.moj-progress-bar > .moj-progress-bar__list > .moj-progress-bar__item')
 }
 
-export const getProgressTrackerCompleted = ($: Root) => {
+export const getProgressTrackerCompleted = ($: CheerioAPI) => {
   return $('.moj-progress-bar > .moj-progress-bar__list > .moj-progress-bar__item > .moj-progress-bar__icon--complete')
 }
 
-export const getArrayItemPropById = ($: Root, id: string, index: number, property: string) => {
+export const getArrayItemPropById = ($: CheerioAPI, id: string, index: number, property: string) => {
   return $(`#${id}\\[${index}\\]\\[${property}\\]`)
 }
 
-export const getGovukTableCell = ($: Root, rowIndex: number, columnIndex: number) => {
+export const getGovukTableCell = ($: CheerioAPI, rowIndex: number, columnIndex: number) => {
   return $(
     `.govuk-table__body > .govuk-table__row:nth-child(${rowIndex}) > .govuk-table__cell:nth-child(${columnIndex})`,
   )
 }
 
-export const getByIdFor = ($: Root, forId: string) => {
+export const getByIdFor = ($: CheerioAPI, forId: string) => {
   return $(`[for=${forId}]`)
 }
