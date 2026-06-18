dc827007602742fd9574dede37a2ca8bbd5e4111
prabash_moj <prabash.balasuriya@justice.gov.uk>
2026-06-18 15:50:23 +0100

    OV-611 : Update content in prisoner search

--- server/routes/journeys/manage/visit/handlers/prisonerSearchSchema.ts
+++ server/routes/journeys/manage/visit/handlers/prisonerSearchSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { createSchema } from '../../../../../middleware/validationMiddleware'
 
-const ERROR_MSG = 'Enter at least 2 characters to search for a prisoner name or prison number'
+const ERROR_MSG = 'Enter at least 2 characters to search for a prisoner name or prisoner number'
 
 export const schema = createSchema({
   searchTerm: z.string({ message: ERROR_MSG }).min(2, { message: ERROR_MSG }),
