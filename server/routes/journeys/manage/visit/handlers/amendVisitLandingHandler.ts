71cb62de175ad45db614dc946a81d0e7ceb308dc
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2026-06-23 13:33:07 +0100

    OV-763 : display notification alert on amend visit page when there are changes to visit status and email notifications are enabled (#277)

--- server/routes/journeys/manage/visit/handlers/amendVisitLandingHandler.ts
+++ server/routes/journeys/manage/visit/handlers/amendVisitLandingHandler.ts
@@ -50,7 +50,7 @@ export default class AmendVisitLandingHandler implements PageHandler {
 
     const visit = await this.officialVisitsService.getOfficialVisitById(Number(ovId), user)
 
-    const [restrictions, prisoner, contacts] = await Promise.all([
+    const [restrictions, prisoner, contacts, visitChangeStatus] = await Promise.all([
       this.personalRelationshipsService.getPrisonerRestrictions(
         visit.prisonerVisited.prisonerNumber,
         0,
@@ -61,6 +61,7 @@ export default class AmendVisitLandingHandler implements PageHandler {
       ),
       this.prisonerService.getPrisonerByPrisonerNumber(visit.prisonerVisited.prisonerNumber, user),
       this.officialVisitsService.getAllContacts(visit.prisonerVisited.prisonerNumber, user),
+      this.officialVisitsService.getVisitChangeStatus(Number(ovId), user),
     ])
 
     const enrichedVisitors = await Promise.all(
@@ -173,6 +174,7 @@ export default class AmendVisitLandingHandler implements PageHandler {
       hasNotApprovedVisitors,
       hasSocialVisitors,
       hasIssueVisitors,
+      hasVisitChanged: visitChangeStatus.hasChanged,
       shouldShowIssues: isFuture(new Date(`${visit.visitDate} ${visit.startTime}`)) && !visit.completionCode,
     })
   }
