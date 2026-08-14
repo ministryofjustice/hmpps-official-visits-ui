1fc418c1e8dc40268b5569ff29925fb410280b02
JackReeveMoJ <jack.reeve@justice.gov.uk>
2026-08-14 15:12:48 +0100

    OV-000: Update frontend libraries (#318)

--- server/utils/restrictionTagColour.test.ts
+++ server/utils/restrictionTagColour.test.ts
@@ -2,7 +2,7 @@ import restrictionTagColour from './restrictionTagColour'
 
 describe('restrictionTagColour', () => {
   it.each([
-    ['ACC', 'govuk-tag--turquoise'],
+    ['ACC', 'govuk-tag--teal'],
     ['BAN', 'govuk-tag--red'],
     ['CCTV', 'govuk-tag--yellow'],
     ['CHILD', 'govuk-tag--grey'],
@@ -10,7 +10,7 @@ describe('restrictionTagColour', () => {
     ['DIHCON', 'govuk-tag--blue'],
     ['NONCON', 'govuk-tag--green'],
     ['PREINF', 'govuk-tag--orange'],
-    ['RESTRICTED', 'govuk-tag--pink'],
+    ['RESTRICTED', 'govuk-tag--magenta'],
     ['FOO', 'govuk-tag--grey'], // default to grey
   ])(
     'should return the restriction type description with "(expired)" if the expiry date is in the past',
